ca1b089a2505808c60747301e6dca76641e0bc85
Danilo Schmidt Bueno <daniloschmidtbueno@gmail.com>
2021-11-21 12:20:05 -0300

    Versionamento da API com a camada de desenvolvimento

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://foodspot:ppKoCn4ZdpLOT3S1hNUyJsmexQ2FAv8a33lMQ0nWqP2CKDNjPfNWP5NegRyswm2sZeN0Cwe5eHcmVPsvBUYqAw==@foodspot.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@foodspot@")
+mongoose.connect("mongodb+srv://admin:123@cluster0.rjlba.mongodb.net/test?authSource=admin&replicaSet=atlas-4qm2jf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 
 module.exports = {
     restaurantemodel: require('./models/restaurante'),
-    cardapiomodel: require('./models/cardapio')
+    cardapiomodel: require('./models/cardapio'),
+    loginmodel: require('./models/login'),
+    loginadmmodel: require('./models/loginadm'),
+    loginRestaurantemodel: require('./models/loginRestaurante')
 }
